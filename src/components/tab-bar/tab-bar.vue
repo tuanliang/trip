19cc7b50651b28822f2967bea126ffdc2deb340a
shiyi <2416976257@qq.com>
2023-07-17 17:24:04 +0800

    tabbar使用vant组件

--- src/components/tab-bar/tab-bar.vue
+++ src/components/tab-bar/tab-bar.vue
@@ -1,60 +1,34 @@
 <template>
   <div class="tab-bar">
-    <div class="tab-bar-item" v-for="(item, index) in tabbarData" :key="index" @click="itemClick(index, item)">
-      <img v-if="currentIndex !== index" :src="getAssetURL(item.image)" alt="">
-      <img v-else :src="getAssetURL(item.imageActive)" alt="">
-      <span class="text" :class="{ active: currentIndex === index }">{{ item.text }}</span>
-    </div>
+    <van-tabbar v-model="currentIndex" active-color="#ff9854">
+      <template v-for="(item, index) in tabbarData" :key="index">
+        <van-tabbar-item icon="home-o" :to="item.path">
+          <span>{{ item.text }}</span>
+          <template #icon>
+            <img v-if="currentIndex !== index" :src="getAssetURL(item.image)" alt="">
+            <img v-else :src="getAssetURL(item.imageActive)" alt="">
+          </template>
+        </van-tabbar-item>
+      </template>
+    </van-tabbar>
+
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue';
+import { getAssetURL } from "@/utils/get_assets.js"
 import tabbarData from '@/assets/data/tabbar'
-import { getAssetURL } from '@/utils/get_assets.js'
-import { useRouter } from 'vue-router';
 
 const currentIndex = ref(0)
 
-const router = useRouter()
-const itemClick = (index, item) => {
-  currentIndex.value = index
-  router.push(item.path)
-}
 
 </script>
 
 <style lang="less" scoped>
 .tab-bar {
-  position: fixed;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  height: 50px;
-  display: flex;
-  border-top: 1px solid #f3f3f3;
-
-  .tab-bar-item {
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-
-    img {
-      width: 36px;
-    }
-
-    .text {
-      font-size: 12px;
-      margin-top: 2px;
-
-      &.active {
-        color: var(--primary-color);
-      }
-
-    }
-
+  img {
+    height: 28px;
   }
 }
 </style>
